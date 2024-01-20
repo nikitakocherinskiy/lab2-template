2b52aaacfd32d6d35c9b4c93d189707cda30e239
n.kocherinskiy <n.kocherinskiy@qiwi.com>
2024-01-20 23:53:26 +0300

    fix: edit deploy

--- postgres/10-create-user.sql
+++ postgres/10-create-user.sql
@@ -2,10 +2,10 @@ CREATE ROLE program WITH PASSWORD 'test';
 ALTER ROLE program WITH LOGIN;
 
 CREATE DATABASE carsservicedb;
-GRANT ALL PRIVILEGES ON DATABASE cars TO program;
+GRANT ALL PRIVILEGES ON DATABASE carsservicedb TO program;
 
 CREATE DATABASE rentalsservicedb;
-GRANT ALL PRIVILEGES ON DATABASE rentals TO program;
+GRANT ALL PRIVILEGES ON DATABASE rentalsservicedb TO program;
 
 CREATE DATABASE paymentsservicedb;
-GRANT ALL PRIVILEGES ON DATABASE payments TO program;
+GRANT ALL PRIVILEGES ON DATABASE paymentsservicedb TO program;
