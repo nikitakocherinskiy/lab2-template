91db8d709fb29f1f9545189c3969fe379298b744
n.kocherinskiy <n.kocherinskiy@qiwi.com>
2024-01-20 23:00:59 +0300

    fix: i hate this shit deploy

--- postgres/10-create-user.sql
+++ postgres/10-create-user.sql
@@ -1,3 +1,11 @@
--- file: 10-create-user.sql
 CREATE ROLE program WITH PASSWORD 'test';
 ALTER ROLE program WITH LOGIN;
+
+CREATE DATABASE carsservicedb;
+GRANT ALL PRIVILEGES ON DATABASE cars TO program;
+
+CREATE DATABASE rentalsservicedb;
+GRANT ALL PRIVILEGES ON DATABASE rentals TO program;
+
+CREATE DATABASE paymentsservicedb;
+GRANT ALL PRIVILEGES ON DATABASE payments TO program;
