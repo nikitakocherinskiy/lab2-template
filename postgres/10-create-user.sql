8e1b584f1e88f14bad1b044c091fb65c48848e7e
n.kocherinskiy <n.kocherinskiy@qiwi.com>
2024-01-21 01:03:16 +0300

    fix: edit db evns

--- postgres/10-create-user.sql
+++ postgres/10-create-user.sql
@@ -1,11 +1,8 @@
-CREATE ROLE program WITH PASSWORD 'test';
-ALTER ROLE program WITH LOGIN;
-
 CREATE DATABASE carsservicedb;
-GRANT ALL PRIVILEGES ON DATABASE carsservicedb TO program;
+GRANT ALL PRIVILEGES ON DATABASE carsservicedb TO postgres;
 
 CREATE DATABASE rentalsservicedb;
-GRANT ALL PRIVILEGES ON DATABASE rentalsservicedb TO program;
+GRANT ALL PRIVILEGES ON DATABASE rentalsservicedb TO postgres;
 
 CREATE DATABASE paymentsservicedb;
-GRANT ALL PRIVILEGES ON DATABASE paymentsservicedb TO program;
+GRANT ALL PRIVILEGES ON DATABASE paymentsservicedb TO postgres;
